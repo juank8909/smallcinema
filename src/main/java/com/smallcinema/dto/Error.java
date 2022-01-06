f40b07d73abc1223d8ca546c1ff4e4235a2fefd1
Juan Camilo Aristizabal <juanaristizabal@seven4n.com>
2022-01-06 10:34:35 -0500

    add first endpoint approach

---
+++ src/main/java/com/smallcinema/dto/Error.java
@@ -0,0 +1,53 @@
+package com.smallcinema.dto;
+
+import org.springframework.validation.annotation.Validated;
+
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Validated
+public class Error {
+    @NotNull
+    private String code;
+    @NotNull
+    private String message;
+
+    public Error(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    public Error() {
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        Error error = (Error) o;
+        return code.equals(error.code) && message.equals(error.message);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(code, message);
+    }
+}
