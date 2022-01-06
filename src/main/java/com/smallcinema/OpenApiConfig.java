aa61dc7c1b4392ce1a250d0e7e60efe1b6c0d4f6
Juan Camilo Aristizabal <juanaristizabal@seven4n.com>
2022-01-06 12:09:21 -0500

    fix application yaml

--- src/main/java/com/smallcinema/OpenApiConfig.java
+++ src/main/java/com/smallcinema/OpenApiConfig.java
@@ -1,9 +1,11 @@
-package com.smallcinema.api.config;
+package com.smallcinema;
 
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class OpenApiConfig {
@@ -13,4 +15,5 @@ public class OpenApiConfig {
         return new OpenAPI().info(new Info().title("Small cinema API Spec")
                 .description("Small cinema API specification (small cinema project)").version("1.0.0"));
     }
+
 }
