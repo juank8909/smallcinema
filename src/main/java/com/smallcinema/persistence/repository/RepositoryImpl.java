74a94ea0b483327ecc95f184aa8fd86246cdb718
Juan Camilo Aristizabal <juanaristizabal@seven4n.com>
2022-01-07 16:09:29 -0500

    fix jooq code geenration

--- src/main/java/com/smallcinema/persistence/repository/RepositoryImpl.java
+++ src/main/java/com/smallcinema/persistence/repository/RepositoryImpl.java
@@ -1,18 +1,28 @@
 package com.smallcinema.persistence.repository;
 
-import com.smallcinema.domain.model.ImmutableMovie;
 import com.smallcinema.domain.model.Movie;
 import com.smallcinema.domain.model.ServiceError;
-import io.vavr.collection.List;
 import io.vavr.control.Either;
 import io.vavr.control.Option;
+import org.jooq.DSLContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static com.smallcinema.persistence.dto.jooq.small_cinema.tables.Movie.*;
+
+import javax.inject.Inject;
 
 @org.springframework.stereotype.Repository
 public class RepositoryImpl implements Repository {
 
     private final Logger log = LoggerFactory.getLogger(RepositoryImpl.class);
+
+    private final DSLContext dsl;
+
+    @Inject
+    public RepositoryImpl(DSLContext dsl) {
+        this.dsl = dsl;
+    }
 
     @Override
     public Either<ServiceError, Movie> updateTimeShowsAndPrices(Movie movie) {
@@ -21,14 +31,9 @@ public class RepositoryImpl implements Repository {
 
     @Override
     public Either<ServiceError, Option<Movie>> getMovie(String movieId) {
-        return Either.right(Option.of(ImmutableMovie
-                .builder()
-                .title("first Movie")
-                .id("123")
-                .price(20.3)
-                .review("excelent movie")
-                .showTimes(List.of("wed 20:00"))
-                .build()));
+        return dsl.select(MOVIE.fields())
+                .from(MOVIE)
+                .where(MOVIE.ID.equals(movieId));
     }
 
     @Override
