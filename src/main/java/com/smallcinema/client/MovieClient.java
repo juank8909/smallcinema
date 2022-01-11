7ddd55166cf3b39e9b0876344eb862aa1e8d80f1
Juan Camilo Aristizabal <juanaristizabal@seven4n.com>
2022-01-11 16:23:42 -0500

    fix client implementation

--- src/main/java/com/smallcinema/client/MovieClient.java
+++ src/main/java/com/smallcinema/client/MovieClient.java
@@ -1,12 +1,15 @@
 package com.smallcinema.client;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.smallcinema.client.dto.OMDbMovieClientDTO;
 import com.smallcinema.domain.model.ServiceError;
-import io.vavr.concurrent.Future;
 import io.vavr.control.Either;
 import io.vavr.control.Option;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
@@ -20,16 +23,25 @@ public class MovieClient {
     private String resource;
 
     public Either<ServiceError, Option<OMDbMovieClientDTO>> getMovie(String movieId) {
-        //todo: make it more functional without if
-        // using Future.of(...)
-        // .map(val -> Either.right(val))
-        // .onFailure(err -> Either.left(new ServiceError("CLIERR, err.getMessage()"))) types are not matching
-        Future<Either<ServiceError, Option<OMDbMovieClientDTO>>> future =
-                Future.of(() -> Option.of(restTemplate.getForObject(resource + movieId, OMDbMovieClientDTO.class)))
-                        .map(Either::right);
-        return future.isFailure()
-                ? Either.left(new ServiceError("CLIERR", "An error occurred when trying to get the movie"))
-                : future.get();
+        try {
+            ResponseEntity<String> response = restTemplate.getForEntity(resource + movieId, String.class);
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode root = mapper.readTree(response.getBody());
+            if (root.path("Response").asText().equals("True"))
+                return Either.right(Option.of(
+                        new OMDbMovieClientDTO(
+                                root.path("Title").asText(),
+                                root.path("Plot").asText(),
+                                root.path("Released").asText(),
+                                root.path("imdbRating").asText(),
+                                root.path("Runtime").asText()
+                        )
+                ));
+            else return Either.right(Option.none());
+        } catch (JsonProcessingException e) {
+            return Either.left(new ServiceError("CLIERR", "An error occurred when trying to get the movie" +
+                    e.getMessage()));
+        }
     }
 
 }
