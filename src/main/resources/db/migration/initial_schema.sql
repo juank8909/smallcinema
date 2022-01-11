c034daa3d6bf92d10fdd26c25323323fe1be0788
Juan Camilo Aristizabal <juanaristizabal@seven4n.com>
2022-01-11 15:03:12 -0500

    add records to initial schema

--- src/main/resources/db/migration/initial_schema.sql
+++ src/main/resources/db/migration/initial_schema.sql
@@ -11,4 +11,28 @@ create table  if not exists "SMALL_CINEMA"."MOVIE"(
 );
 
 insert into "SMALL_CINEMA"."MOVIE" ("ID","TITLE","SHOW_TIMES","PRICE","REVIEW")
-VALUES ('tt0232500','The Fast and the Furious',ARRAY [ 'wed 8:00 PM','SAT 9:00 PM' ], 14, 5 )
+VALUES ('tt0232500','The Fast and the Furious',ARRAY [ 'wed 8:00 PM','SAT 9:00 PM' ], 14, 5 );
+
+insert into "SMALL_CINEMA"."MOVIE" ("ID","TITLE","SHOW_TIMES","PRICE","REVIEW")
+VALUES ('tt0322259','2 Fast 2 Furious',ARRAY [ 'wed 8:00 PM','SAT 9:00 PM' ], 14, 5 );
+
+insert into "SMALL_CINEMA"."MOVIE" ("ID","TITLE","SHOW_TIMES","PRICE","REVIEW")
+VALUES ('tt0463985','The Fast and the Furious: Tokyo Drift',ARRAY [ 'wed 8:00 PM','SAT 9:00 PM' ], 14, 5 );
+
+insert into "SMALL_CINEMA"."MOVIE" ("ID","TITLE","SHOW_TIMES","PRICE","REVIEW")
+VALUES ('tt1013752','Fast & Furious',ARRAY [ 'wed 8:00 PM','SAT 9:00 PM' ], 14, 5 );
+
+insert into "SMALL_CINEMA"."MOVIE" ("ID","TITLE","SHOW_TIMES","PRICE","REVIEW")
+VALUES ('tt1596343','Fast Five',ARRAY [ 'wed 8:00 PM','SAT 9:00 PM' ], 14, 5 );
+
+insert into "SMALL_CINEMA"."MOVIE" ("ID","TITLE","SHOW_TIMES","PRICE","REVIEW")
+VALUES ('tt1905041','Fast & Furious 6',ARRAY [ 'wed 8:00 PM','SAT 9:00 PM' ], 14, 5 );
+
+insert into "SMALL_CINEMA"."MOVIE" ("ID","TITLE","SHOW_TIMES","PRICE","REVIEW")
+VALUES ('tt2820852','Furious 7',ARRAY [ 'wed 8:00 PM','SAT 9:00 PM' ], 14, 5 );
+
+insert into "SMALL_CINEMA"."MOVIE" ("ID","TITLE","SHOW_TIMES","PRICE","REVIEW")
+VALUES ('tt4630562','The Fate of the Furious',ARRAY [ 'wed 8:00 PM','SAT 9:00 PM' ], 14, 5 );
+
+insert into "SMALL_CINEMA"."MOVIE" ("ID","TITLE","SHOW_TIMES","PRICE","REVIEW")
+VALUES ('tt5433138','F9: The Fast Saga',ARRAY [ 'wed 8:00 PM','SAT 9:00 PM' ], 14, 5 );
